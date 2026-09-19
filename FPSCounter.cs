3d61d352751a35dc74636217eb6160b7418047e5
GSQ123 <162472720+GSQ12345@users.noreply.github.com>
2026-09-19 14:33:17 +0200

    added files wow

---
+++ FPSCounter.cs
@@ -0,0 +1,47 @@
+﻿using MelonLoader;
+using System;
+using UnityEngine;
+
+[assembly: MelonInfo(typeof(FPSCounter), "FPS Counter", "1.0.0", "GSQ123")]
+
+public class FPSCounter : MelonMod
+{
+    private float fps;
+    private float fpsTimer;
+
+    private GUIStyle style;
+
+    public override void OnInitializeMelon()
+    {
+        MelonLogger.Msg("FPS Counter loaded!");
+    }
+
+    public override void OnUpdate()
+    {
+        fpsTimer += Time.unscaledDeltaTime;
+
+        if (fpsTimer >= 0.25f)
+        {
+            fps = 1f / Time.unscaledDeltaTime;
+            fpsTimer = 0f;
+        }
+    }
+
+    public override void OnGUI()
+    {
+        if (style == null)
+        {
+            style = new GUIStyle(GUI.skin.label);
+            style.fontSize = 20;
+            style.fontStyle = FontStyle.Bold;
+            style.alignment = TextAnchor.UpperLeft;
+            style.normal.textColor = Color.white;
+        }
+
+        GUI.Label(
+            new Rect(10, 10, 150, 30),
+            "FPS: " + Mathf.RoundToInt(fps),
+            style
+        );
+    }
+}
